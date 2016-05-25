0b8d1e3065ac536fc053c3ca523dcf2aee2c84d9
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-05-25 10:07:42 +0200

    add LocalShop

---
+++ lib/rawbotz/local_shop.rb
@@ -0,0 +1,12 @@
+module Rawbotz
+  module LocalShop
+    def self.product_page_frontend(product, settings)
+      # TODO settings could be memoized
+      "#{settings['local_shop']['base_uri']}catalog/product/view/id/#{product.product_id}"
+    end
+    def self.product_page_backend(product, settings)
+      # TODO settings could be memoized
+      "#{settings['local_shop']['base_uri']}index.php/rawadmin/catalog_product/edit/id/#{product.product_id}"
+    end
+  end
+end
