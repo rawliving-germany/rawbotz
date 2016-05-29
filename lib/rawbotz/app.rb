abef6f7cffbb8a2b69299d228bc974ba4feb53dc
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-05-29 23:25:04 +0200

    fix remote_order_display

--- lib/rawbotz/app.rb
+++ lib/rawbotz/app.rb
@@ -240,7 +240,7 @@ class RawbotzApp < Sinatra::Base
     @remote_order_id = params[:id]
     @remote_order_items = Rawbotz.mech.products_from_order params[:id]
     @remote_products_qty = @remote_order_items.map do |remote_product|
-      [RemoteProduct.find_by(name: remote_product[1]) || OpenStruct.new(name: remote_product[1]), remote_product[2]]
+      [RemoteProduct.find_by(name: remote_product[0]) || OpenStruct.new(name: remote_product[0]), remote_product[2]]
     end
     haml "remote_order/view".to_sym
   end
