43304da3e90a63124bcc903016c13c16b1f7193e
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-05-31 09:08:44 +0200

    reflect IconHelpers module change

--- lib/rawbotz/helpers/icon_helper.rb
+++ lib/rawbotz/helpers/icon_helper.rb
@@ -1,19 +1,24 @@
 module Rawbotz
-  module IconHelper
-    def order_icon
-      '<i class="fa fa-shopping-cart"></i>'
-    end
-    def product_icon
-      '<i class="fa fa-cube"></i>'
-    end
-    def products_icon
-      '<i class="fa fa-cubes"></i>'
-    end
-    def index_icon
-      '<i class="fa fa-th-list"></i>'
-    end
-    def link_icon
-      '<i class="fa fa-link"></i>'
+  module Helpers
+    module IconHelper
+      def order_icon
+        '<i class="fa fa-shopping-cart"></i>'
+      end
+      def product_icon
+        '<i class="fa fa-cube"></i>'
+      end
+      def products_icon
+        '<i class="fa fa-cubes"></i>'
+      end
+      def index_icon
+        '<i class="fa fa-th-list"></i>'
+      end
+      def link_icon
+        '<i class="fa fa-link"></i>'
+      end
+      def remote_icon
+        '<i class="fa fa-globe"></i>'
+      end
     end
   end
 end
