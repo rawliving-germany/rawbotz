1278ac111531153d95f6aa0bbe844a143698fda3
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-06-06 20:30:06 +0200

    remove obsolete options hash in link_import script.

---
+++ lib/rawbotz/routes/suppliers.rb
@@ -0,0 +1,29 @@
+require 'rawbotz/routes'
+
+module Rawbotz::RawbotzApp::Routing::Suppliers
+  include RawgentoModels
+
+  def self.registered(app)
+    # app.get  '/supplier/:id', &show_supplier
+    show_supplier = lambda do
+      @supplier = Supplier.find(params[:id])
+      haml "supplier/view".to_sym
+    end
+
+    # app.post '/supplier/:id', &update_supplier
+    update_supplier = lambda do
+      @supplier = Supplier.find(params[:id])
+      @supplier.email = params[:email]
+      @supplier.order_template = params[:order_template]
+      if @supplier.save
+        add_flash :success, "Supplier updated"
+      else
+        add_flash :error, "Supplier could not be saved"
+      end
+      haml "supplier/view".to_sym
+    end
+
+    app.get  '/supplier/:id', &show_supplier
+    app.post '/supplier/:id', &update_supplier
+  end
+end
