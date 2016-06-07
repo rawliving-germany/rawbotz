096a992f15b4cfcbe391842e3b9eb181fed7676d
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-06-07 14:25:22 +0200

    improve datapolate implementation (preparation for chart.js 2)

--- lib/rawbotz/datapolate.rb
+++ lib/rawbotz/datapolate.rb
@@ -6,11 +6,11 @@ module Rawbotz
       stock_first, stock_last = stock.minmax_by{|s| s.date}
       first_dates = []
       first_dates << sales_first[0]   if !sales_first.nil?
-      first_dates << stock_first.date if !stock_first.nil?
+      first_dates << stock_first.date.to_date if !stock_first.nil?
 
       last_dates = []
       last_dates << sales_last[0]   if !sales_last.nil?
-      last_dates << stock_last.date if !stock_last.nil?
+      last_dates << stock_last.date.to_date if !stock_last.nil?
 
       [first_dates.min, last_dates.max]
     end
@@ -23,11 +23,17 @@ module Rawbotz
       # from [date, sales] and [date, stock] create realistic list
       from_date, to_date = date_minmax sales, stock
       days = explode_days from_date, to_date
+
+      stock_data = Hash.new(nil)
+      stock.each{|s| stock_data[s.date.to_date] = s.qty}
+      sales_data = Hash.new(nil)
+      sales.each{|s| sales_data[s[0].to_date] = s.fetch(1)}
+
       data = {}
       days.each do |day|
-        data[day] = {label: day,
-                     stock: stock.find{|s| s.date.to_date == day}.try(:qty) || 0,
-                     sales: sales.find{|s| s[0].to_date == day}.try(:fetch, 1) || 0
+        data[day] = {label: day.to_date,
+                     stock: stock_data[day],
+                     sales: sales_data[day]
         }
       end
       data
