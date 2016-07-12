04850d2db80164c9fa888af935c2327bf75d6787
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-07-12 17:32:46 +0200

    add test for mail templates new SUPPLIERSKU (#1)

--- test/mail_template_test.rb
+++ test/mail_template_test.rb
@@ -9,19 +9,19 @@ class MailTemplateTest < MiniTest::Test
       "SUBJECT=I mail you SUPPLIERNAME\n"\
       "\n"\
       " \n"\
-      "* PRODUCTCODE QTY (NUM_PACKS of PACKSIZE) PRODUCTNAME\n"\
+      "* SUPPLIERSKU QTY (NUM_PACKS of PACKSIZE) PRODUCTNAME\n"\
       "see you"
     order = {supplier: {name: "Suppliername"}, order_items: [
       {num_wished: 1, local_product: {name: "First Product", packsize: 4}},
-      {num_wished: 5, local_product: {name: "Second Product", packsize: 5}},
-      {num_wished: 7, local_product: {name: "Third Product"}}
+      {num_wished: 5, local_product: {name: "Second Product", packsize: 5, supplier_sku: 'sku1', supplier_prod_name: 'Suppliers first'}},
+      {num_wished: 7, local_product: {name: "Third Product", supplier_sku: ''}}
     ]}
 
     expected = "Dear Suppliername\n"\
       "\n"\
       " \n"\
       " 1 (0.25 of 4) First Product\n"\
-      " 5 (1.0 of 5) Second Product\n"\
+      "sku1 5 (1.0 of 5) Suppliers first\n"\
       " 7 ( of ) Third Product\n"\
       "see you"
     result = Rawbotz::MailTemplate.consume(template, order)
