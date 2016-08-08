8903003637a19623a51a4bd3d332857de7ac1d1f
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-08-08 11:47:20 +0200

    refactored icon_helper to accept tooltip, new icon helper methods.

--- lib/rawbotz/helpers/icon_helper.rb
+++ lib/rawbotz/helpers/icon_helper.rb
@@ -1,62 +1,96 @@
 module Rawbotz
   module Helpers
     module IconHelper
-      def add_icon
-        '<i class="fa fa-plus"></i>'
+      def add_icon tooltip_text: nil
+        icon "plus", tooltip_text: tooltip_text
       end
-      def error_icon
-        '<i class="fa fa-flash"></i>'
+      def error_icon tooltip_text: nil
+        icon "flash", tooltip_text: tooltip_text
       end
-      def index_icon
-        '<i class="fa fa-th-list"></i>'
+      def index_icon tooltip_text: nil
+        icon "th-list", tooltip_text: tooltip_text
       end
-      def info_icon
-        '<i class="fa fa-info-circle"></i>'
+      def info_icon tooltip_text: nil
+        icon "info-circle", tooltip_text: tooltip_text
       end
-      def link_icon
-        '<i class="fa fa-link"></i>'
+      def link_icon tooltip_text: nil
+        icon "info-link", tooltip_text: tooltip_text
       end
-      def order_icon
-        '<i class="fa fa-shopping-cart"></i>'
+      def order_icon tooltip_text: nil
+        icon "shopping-cart", tooltip_text: tooltip_text
       end
-      def packsize_icon
-        '<i class="fa fa-cube"></i>'
+      def packlist_icon tooltip_text: nil
+        icon "paperclip", tooltip_text: tooltip_text
       end
-      def product_icon
-        '<i class="fa fa-cube"></i>'
+      def packsize_icon tooltip_text: nil
+        icon "cube", tooltip_text: tooltip_text
       end
-      def products_icon
-        '<i class="fa fa-cubes"></i>'
+      def product_icon tooltip_text: nil
+        icon "cube", tooltip_text: tooltip_text
       end
-      def remote_icon
-        '<i class="fa fa-globe"></i>'
+      def products_icon tooltip_text: nil
+        icon "cubes", tooltip_text: tooltip_text
       end
-      def sales_icon
-        '<i class="fa fa-shopping-cart"></i>'
+      def remote_icon tooltip_text: nil
+        icon "globe", tooltip_text: tooltip_text
       end
-      def save_icon
-        '<i class="fa fa-envelope"></i>'
+      def sales_icon tooltip_text: nil
+        icon "shopping-cart", tooltip_text: tooltip_text
       end
-      def settings_icon
-        '<i class="fa fa-wrench"></i>'
+      def save_icon tooltip_text: nil
+        icon "envelope", tooltip_text: tooltip_text
       end
-      def stock_empty_icon
-        '<i class="fa fa-battery-0"></i>'
+      def settings_icon tooltip_text: nil
+        icon "wrench", tooltip_text: tooltip_text
       end
-      def stock_icon
-        '<i class="fa fa-battery-2"></i>'
+      def shelve_icon tooltip_text: nil
+        icon "map-signs", tooltip_text: tooltip_text
       end
-      def stock_full_icon
-        '<i class="fa fa-battery-4"></i>'
+      def stock_empty_icon tooltip_text: nil
+        icon "battery-0", tooltip_text: tooltip_text
       end
-      def success_icon
-        '<i class="fa fa-smile-o"></i>'
+      def stock_icon tooltip_text: nil
+        icon "battery-2", tooltip_text: tooltip_text
       end
-      def supplier_icon
-        '<i class="fa fa-truck"></i>'
+      def stock_full_icon tooltip_text: nil
+        icon "battery-4", tooltip_text: tooltip_text
       end
-      def warning_icon
-        '<i class="fa fa-warning"></i>'
+      def success_icon tooltip_text: nil
+        icon "smile-o", tooltip_text: tooltip_text
+      end
+      def supplier_icon tooltip_text: nil
+        icon "truck", tooltip_text: tooltip_text
+      end
+      def view_icon tooltip_text: nil
+        icon "eye", tooltip_text: tooltip_text
+      end
+      def warning_icon tooltip_text: nil
+        icon "warning", tooltip_text: tooltip_text
+      end
+
+      def order_state_icon order
+        case order.state
+        when 'new'
+          icon "star"
+        when 'mailed'
+          icon "envelope-o"
+        when 'ordered'
+          icon "arrow-right"
+        when 'deleted'
+          icon "remove"
+        else
+          icon "question"
+        end
+      end
+
+      private
+      def icon fa_name, tooltip_text: nil
+        "<i class=\"fa fa-#{fa_name}\"%s></i>" % tooltip(tooltip_text)
+      end
+      def tooltip tooltip_text
+        if tooltip_text
+          " title=\"#{tooltip_text}\""
+        end
       end
     end
   end
