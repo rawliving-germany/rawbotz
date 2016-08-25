b89831df8d59e9d3ac41d9d62b591ae10489734e
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-08-25 09:48:46 +0200

    StockProductFactory#create_single implementation

--- lib/rawbotz/models/stock_product_factory.rb
+++ lib/rawbotz/models/stock_product_factory.rb
@@ -4,12 +4,38 @@ module Rawbotz
       include RawgentoModels
       include RawgentoDB
 
+      def self.create_single product
+        product_id = product.product_id
+        sales_30  = Query.num_sales_since(Date.today - 30,  product_id)
+        sales_60  = Query.num_sales_since(Date.today - 60,  product_id)
+        sales_90  = Query.num_sales_since(Date.today - 90,  product_id)
+        sales_365 = Query.num_sales_since(Date.today - 365, product_id)
+
+        beginning_of_time = StockItem.order(created_at: :asc).pluck(:created_at).first.to_date
+
+        sales       = Query.num_sales_since(beginning_of_time, product_id)
+        first_sales = Query.first_sales(product_id)
+        # Default to today instead of nil date
+        first_sales.default = Date.today
+
+        stock = Query.stock.find {|s| s.product_id = product_id}.qty
+        StockProduct.new(product: LocalProduct.find_by(product_id: product_id),
+                         current_stock: stock[product_id],
+                         sales_last_30: sales_30[product_id],
+                         sales_last_60: sales_60[product_id],
+                         sales_last_90: sales_90[product_id],
+                         sales_last_365: sales_365[product_id],
+                         num_days_first_sale: Date.today - first_sales[product_id].to_date
+                        )
+
+      end
+
       def self.create suppliers
         product_ids = LocalProduct.where(supplier_id: suppliers.map(&:id), active: true).pluck(:product_id)
 
-        sales_30 = Query.num_sales_since(Date.today - 30, product_ids)
-        sales_60 = Query.num_sales_since(Date.today - 60, product_ids)
-        sales_90 = Query.num_sales_since(Date.today - 90, product_ids)
+        sales_30  = Query.num_sales_since(Date.today - 30,  product_ids)
+        sales_60  = Query.num_sales_since(Date.today - 60,  product_ids)
+        sales_90  = Query.num_sales_since(Date.today - 90,  product_ids)
         sales_365 = Query.num_sales_since(Date.today - 365, product_ids)
 
         beginning_of_time = StockItem.order(created_at: :asc).first.created_at.to_date
@@ -35,4 +61,3 @@ module Rawbotz
     end
   end
 end
-
