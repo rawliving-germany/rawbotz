bb4c5ca4b208f7d87944c045fb34d1378f52c255
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-09-07 15:47:08 +0200

    accept multiple messages in add_flash

--- lib/rawbotz/helpers/flash_helper.rb
+++ lib/rawbotz/helpers/flash_helper.rb
@@ -4,9 +4,11 @@ module Rawbotz
       def flash
         @flash = session[:flash]# || {}
       end
-      def add_flash kind, msg
-        session[:flash] ||= {}
-        (session[:flash][kind] ||= []) << msg
+      def add_flash kind, *messages
+        messages.each do |msg|
+          session[:flash] ||= {}
+          (session[:flash][kind] ||= []) << msg
+        end
       end
     end
   end
